bee45879bfb2b4c493070d28529a14ddf547194e
arvi9 <adc9work@gmail.com>
2017-08-20 16:45:04 +0530

    comp

---
+++ src/test/programme/Test.java
@@ -0,0 +1,13 @@
+package test.programme;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Hi
+		for (int i = 0; i <5 ; i++) {
+			System.out.println("Hi");
+		}
+
+	}
+
+}
